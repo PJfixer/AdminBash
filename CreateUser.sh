d1adb1252ac60618df0d3ee993a4e39d479ecca7
Pierre JEANNE <pierre.jeanne96@gmail.com>
2019-10-30 15:48:35 +0100

    fix chmod

--- CreateUser.sh
+++ CreateUser.sh
@@ -2,22 +2,17 @@
 
 # $1 = new username
 # $2 = size allocated for the new user in Mo
-# $3 = is the new user allowed to hava acces to the shared directories
 
 useradd $1 -m    -p $(openssl passwd -1 ChangeMe) && \
 passwd -e $1 && \
 lvcreate -n $1 -L $2m vg01 && \
 mkfs -t ext4 /dev/vg01/$1 && \
+
 echo "/dev/vg01/$1  /home/$1 ext4 defaults 0  0" >> /etc/fstab && \
 mount -a && \
 cp -RT /etc/skel/ /home/$1 && \
-chown -Rf $1:$1 /home/$1  && \
-chmod  -R 760 /home/$1
-if [ $3 == "yes" ]; then
-    	echo "Giving acces to shared directories"
-	usermod -a -G share $1
-	ln -s /home/share /home/$1/share
-	
-fi
+chown -R $1:$1 /home/$1  && \
+chmod  -R 770 /home/$1
+
 
 echo "new user created"
--- RemoveUser.sh
+++ RemoveUser.sh
@@ -5,9 +5,11 @@
 
 
 
+
 umount /home/$1
+rm /home/$1/share
 rm -Rf /home/$1
 lvremove /dev/vg01/$1
 awk '!/'$1'/' /etc/fstab > temp && mv temp /etc/fstab 
 userdel $1
-
+umount /home/$1 
