25344f416577bef81f02cd5a907ba0dbc4e1e1a6
Pierre JEANNE <pierre.jeanne96@gmail.com>
2019-10-30 14:41:36 +0100

    fix chmod

--- CreateUser.sh
+++ CreateUser.sh
@@ -11,13 +11,13 @@ mkfs -t ext4 /dev/vg01/$1 && \
 echo "/dev/vg01/$1  /home/$1 ext4 defaults 0  0" >> /etc/fstab && \
 mount -a && \
 cp -RT /etc/skel/ /home/$1 && \
-chown -Rf $1:$1 /home/$1
+chown -Rf $1:$1 /home/$1  && \
+chmod  -R 760 /home/$1
 if [ $3 == "yes" ]; then
     	echo "Giving acces to shared directories"
 	usermod -a -G share $1
 	ln -s /home/share /home/$1/share
-	chmod 760 /home/$1/share
-	chown $1 /home/$1/share
+	
 fi
 
 echo "new user created"
